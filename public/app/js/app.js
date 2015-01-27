f543ff663b820859cb5edb48e0f5962fedd85b98
santiago cotto <taioloco@gmail.com>
2015-01-27 16:21:46 -0200

    project structure | adding dependencies node and angular

---
+++ public/app/js/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('myApp', [
+    'ngRoute'
+]);
+
+
+app.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/home', {
+                templateUrl: 'partials/home.html',
+                controller: 'HomeCtrl'
+            }).
+            otherwise({
+                redirectTo: '/home'
+            });
+    }]);
+
+
+app.controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    console.log("HomeCtrl");
+}]);
