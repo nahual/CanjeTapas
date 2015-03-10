cc8fbb2aec663d28d98b1b742d54c3e4fa76bac1
Gervasio Marchand <gmc@gmc.uy>
2015-03-10 11:42:43 -0300

    Arreglos a partir de los bugs reportados en el primer encuentro

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -23,15 +23,7 @@ app.controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.sendForm = function () {
 
-        // Números negativos
-        $scope.item.monto = Math.round(Math.abs($scope.item.monto));
-        $scope.item.tapitas = Math.round(Math.abs($scope.item.tapitas));
-
-        if ($scope.item.monto > 10000 || $scope.item.tapitas > 10000) {
-            window.location = 'http://localhost:9999'
-        }
-
-        if ($scope.item.tapitas == 0) {
+        if ($scope.item.tapitas < 0) {
             $scope.message.success = "Puedes canjear 999999 productos";
             return;
         }
@@ -40,7 +32,7 @@ app.controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
             $scope.message.success = "No tienes dinero suficiente para canjear el pprorducto :(";
         }
         else if ($scope.item.tapitas < 10 || undefined == $scope.item.tapitas) {
-            $scope.message.success = "No tienes dinero suficientes para canjear el producto :(";
+            $scope.message.success = "No tienes tapitas suficientes para canjear el producto :(";
         }
         // Dinero >> tapitas => tapitas determina cantidad de productos (sobra dinero)
         else if ($scope.item.monto / 10 >= $scope.item.tapitas) {
@@ -74,8 +66,5 @@ app.controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
     };
 
     $scope.cancelForm = function () {
-        //TODO change for real function or let it be :)
-        $scope.message.success = null;
-        $scope.message.error = "Ha ocurrido un error inesperado";
     };
 }]);
