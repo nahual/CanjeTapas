c39a03d01793b91111f399c80bcda809bc11b8d8
Gervasio Marchand <gmc@gmc.uy>
2015-09-04 15:02:06 -0300

    Cambios hechos por @johnblanco para el segundo encuentro

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,8 +1,6 @@
 var app = angular.module('myApp', [
     'ngRoute'
 ]);
-
-
 app.config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider.
@@ -14,68 +12,41 @@ app.config(['$routeProvider',
                 redirectTo: '/home'
             });
     }]);
-
-
 app.controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
-
     $scope.item = {};
     $scope.message = {};
-
     $scope.sendForm = function () {
-
-        // Números negativos
-        $scope.item.monto = Math.round(Math.abs($scope.item.monto));
-        $scope.item.tapitas = Math.round(Math.abs($scope.item.tapitas));
-
-        if ($scope.item.monto > 10000 || $scope.item.tapitas > 10000) {
-            window.location = 'http://localhost:9999'
-        }
-
-        if ($scope.item.tapitas == 0) {
-            $scope.message.success = "Puedes canjear 999999 productos";
-            return;
-        }
-
         if ($scope.item.monto < 100 || undefined == $scope.item.monto) {
-            $scope.message.success = "No tienes dinero suficiente para canjear el pprorducto :(";
+            $scope.message.success = "No tienes dinero suficiente para canjear el producto :(";
         }
         else if ($scope.item.tapitas < 10 || undefined == $scope.item.tapitas) {
-            $scope.message.success = "No tienes dinero suficientes para canjear el producto :(";
+            $scope.message.success = "No tienes tapitas suficientes para canjear el producto :(";
         }
         // Dinero >> tapitas => tapitas determina cantidad de productos (sobra dinero)
         else if ($scope.item.monto / 10 >= $scope.item.tapitas) {
-
             var itemsTapitas = Math.floor($scope.item.tapitas / 10);
-            var dineroSobra = Math.floor($scope.item.monto - ($scope.item.tapitas * 10));
+            var dineroSobra = $scope.item.monto - 100 * itemsTapitas;
             var msgSobra = '';
-
             if (dineroSobra > 0) {
                 msgSobra = " y te sobran $" + dineroSobra;
             }
-
             $scope.message.success = "Puedes canjear " + itemsTapitas + " productos" + msgSobra;
         }
         // Tapitas >> dinero (sobran tapitas)
         else{
-
             var itemsMonto = Math.floor($scope.item.monto / 100);
             var tapitasSobra = Math.floor($scope.item.tapitas - (itemsMonto * 10));
             var msgSobra = '';
-
             if (tapitasSobra > 0) {
                 msgSobra = " y te sobran " + tapitasSobra + " tapitas";
             }
-
             $scope.message.success = "Puedes canjear " + itemsMonto + " productos" + msgSobra;
         }
-
         $scope.message.error = null;
-
     };
-
     $scope.cancelForm = function () {
-        //TODO change for real function or let it be :)
-        $scope.message.success = null;
-        $scope.message.error = "Ha ocurrido un error inesperado";
+        $scope.item.monto = '';
+        $scope.item.tapitas = '';
     };
 }]);
+
