17c8227f623b69236a396b446e8474fba09a9467
Gervasio Marchand <gmc@gmc.uy>
2015-09-12 01:33:45 -0300

    Agregando envases (y algunos errores)

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -40,67 +40,52 @@ app.controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
             return
         }
 
-        if ($scope.item.monto < 100 || undefined == $scope.item.monto) {
-            $scope.message.success = "No tienes dinero suficiente para canjear el producto :(";
+        if (!isNumeric($scope.item.envases)){
+            $scope.message.error = "La cantidad de envases no es un número";
+            return
         }
-        else if ($scope.item.tapitas < 10 || undefined == $scope.item.tapitas) {
-            $scope.message.success = "No tienes tapitas suficientes para canjear el producto :(";
-        }
-        else if ($scope.item.monto > 10000) {
-            $scope.message.success = "Con tanta plata, mejor usala para otra cosa... no canjeamos más de $10.000";
-        }
-        else if ($scope.item.tapitas > 10000) {
-            $scope.message.success = "No se aceptan canjes con más de 10.000 tapitas";
-        }
-        // Dinero >> tapitas => tapitas determina cantidad de productos (sobra dinero)
-        else if ($scope.item.monto / 10 >= $scope.item.tapitas) {
 
-            var itemsTapitas = Math.floor($scope.item.tapitas / 10);
-            var dineroSobra = Math.floor($scope.item.monto - (itemsTapitas * 100));
-            var tapitasSobra = $scope.item.tapitas - itemsTapitas * 10;
-            var msgSobra = '';
+        var maximo_canje_envases = Math.min(Math.floor($scope.item.monto / 150), Math.floor($scope.item.envases / 5));
+        var max_actual = [0, 0]; //aca guardo los canjes hechos con tapas y los hechos con envases
 
-            if (dineroSobra > 0) {
-                msgSobra = ", te sobran $" + dineroSobra;
-                if (tapitasSobra > 0) {
-                    msgSobra += " y";
-                }
+        for (i = 0; i <= maximo_canje_envases; i++){
+            var dinero_sobrante = $scope.item.monto - i * 150;
+            var canjes_tapas = Math.min(Math.floor(dinero_sobrante / 100), Math.floor($scope.item.tapitas / 10));
+            if (max_actual[0] + max_actual[1] < i + canjes_tapas) {
+                max_actual = [canjes_tapas, i]
             }
-            if (tapitasSobra > 0) {
-                msgSobra += " te sobran " + tapitasSobra + " tapitas";
-            }
-
-            $scope.message.success = "Puedes canjear " + itemsTapitas + " productos" + msgSobra;
-        }
-        // Tapitas >> dinero (sobran tapitas)
-        else{
-
-            var itemsMonto = Math.floor($scope.item.monto / 100);
-            var tapitasSobra = Math.floor($scope.item.tapitas - (itemsMonto * 10));
-            var dineroSobra = $scope.item.monto - itemsMonto * 100;
-            var msgSobra = '';
-
-            if (dineroSobra > 0) {
-                msgSobra = ", te sobran $" + dineroSobra;
-                if (tapitasSobra > 0) {
-                    msgSobra += " y";
-                }
-            }
-            if (tapitasSobra > 0) {
-                msgSobra += " te sobran " + tapitasSobra + " tapitas";
-            }
-
-            $scope.message.success = "Puedes canjear " + itemsMonto + " productos" + msgSobra;
         }
 
+        var vuelto_dinero = $scope.item.monto - max_actual[0] * 100 - max_actual[1] * 150;
+        var vuelto_tapas = $scope.item.envases - max_actual[1] * 15;
+        var vuelto_envases = $scope.item.tapitas - max_actual[0] * 10;
+
+        var vuelto = [];
+        if (vuelto_dinero > 0) {
+            vuelto.push('$' + vuelto_dinero)
+        }
+        if (vuelto_tapas > 0) {
+            vuelto.push(vuelto_tapas + ' tapa' + (vuelto_tapas > 1 ? 's' : ''))
+        }
+        if (vuelto_envases > 0) {
+            vuelto.push(vuelto_tapas + ' envase' + (vuelto_envases > 1 ? 's' : ''))
+        }
+
+        var vuelto_mensaje = '';
+        if (vuelto.length == 0) {
+            vuelto_mensaje = ''
+        } else if (vuelto.length == 1) {
+            vuelto_mensaje = ', te sobran ' + vuelto[0]
+        } else {
+            vuelto_mensaje = ', te sobran ' + vuelto.slice(0, vuelto.length - 1).join(', ') + ' y ' + vuelto[vuelto.length - 1];
+        }
+        $scope.message.success = 'Puedes canjear ' + (max_actual[0] + max_actual[1]) + ' productos (' + max_actual[0] +' canjes con tapas y ' + max_actual[1] +' canjes con envases)' + vuelto_mensaje;
         $scope.message.error = null;
 
     };
 
     $scope.cancelForm = function () {
-        $scope.item.monto = '';
-        $scope.item.tapitas = '';
-        $scope.message.error = null;
+        $scope.message.error = "Ha ocurrido un error inesperado";
         $scope.message.success = null;
     };
 }]);
